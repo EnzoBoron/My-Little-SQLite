0f7e99fca9488f604c725ee3cdbccb43c0ad5fee
Enzo Bo <enzo1.boron@gmail.com>
2026-03-11 17:44:21 +0100

    [~] Small fixes

--- header/sqlite.hpp
+++ header/sqlite.hpp
@@ -1,4 +1,22 @@
 #ifndef SQLITE_HPP
 #define SQLITE_HPP
 
+    #include <iostream>
+    #include <string>
+    #include <vector>
+
+    struct file
+    {
+        std::string name;
+    };
+
+    struct s_state
+    {
+        bool is_working;
+    };
+
+    void main_loop(void);
+    void main_parser(std::vector<std::string>);
+    
+
 #endif /* SQLITE_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include "../header/sqlite.hpp"
 
 int main(int ac, char**av) {
+    file *m_file = new file();
+    if (ac == 1) {
+        m_file->name = "test";
+    } else if (ac == 2) {
+        std::cout << av[0] << " " << av[1] << std::endl;
+        return 0;
+    } else {
+        return 84;
+    }
+
+    main_loop();
+
     return 0;
 }
