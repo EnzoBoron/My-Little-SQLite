202589a403ede7d94c1f146b017de83c0d969d96
Enzo Bo <enzo1.boron@gmail.com>
2026-03-11 17:41:36 +0100

    [+] Add the main loop

---
+++ src/loop.cpp
@@ -0,0 +1,30 @@
+#include "../header/sqlite.hpp"
+
+std::vector<std::string> process_remaining_lines(std::string instruction, std::vector<std::string> bloc) {
+     while (instruction[instruction.length() - 1] != ';') {
+            std::cout << "   ...> ";
+            getline(std::cin, instruction);
+            bloc.push_back(instruction);
+        }
+    return bloc;
+}
+
+void main_loop(void) {
+    std::string instruction;
+    std::vector<std::string> bloc;
+    s_state *state = new s_state;
+    state->is_working = true;
+
+    std::cout << "SQLite version 2.8.17" << std::endl;
+    std::cout << "Enter \".help\" for instructions" << std::endl;
+
+    while (state->is_working) {
+        std::cout << "sqlite> ";
+        getline(std::cin, instruction);
+        bloc.push_back(instruction);
+        if (instruction[0] != '.') {
+            process_remaining_lines(instruction, bloc);
+        }
+        main_parser(bloc);
+    }
+}
