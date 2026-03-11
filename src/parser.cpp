684a2e5b1da61883727458accf6b544f0ab7343b
Enzo Bo <enzo1.boron@gmail.com>
2026-03-11 17:58:56 +0100

    [+] Add exit and quit commands

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,13 +1,16 @@
 #include "../header/sqlite.hpp"
 
 void main_parser(std::vector<std::string> bloc) {
-
-}
-
-void query_parser(std::vector<std::string> bloc) {
     
 }
 
-void general_parsing(std::vector<std::string> bloc) {
+void query_parser(std::string line) {
+    if (line == ".quit" || line == ".exit")
+        exit(0);
+}
 
+void general_parser(std::vector<std::string> bloc) {
+    if (bloc[0][0] == '.') {
+            query_parser(bloc[0]);
+    }
 }
