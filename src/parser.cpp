9e1b094446751f2bf2006b9e9e13ca99fead29a7
Enzo Bo <enzo1.boron@gmail.com>
2026-03-11 17:43:02 +0100

    [+] Add functions to the parsing

---
+++ src/parser.cpp
@@ -0,0 +1,13 @@
+#include "../header/sqlite.hpp"
+
+void main_parser(std::vector<std::string> bloc) {
+
+}
+
+void query_parser(std::vector<std::string> bloc) {
+    
+}
+
+void general_parsing(std::vector<std::string> bloc) {
+
+}
